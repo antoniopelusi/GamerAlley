76b34a3c9c4997bc49c47272602d3b58888cc36b
Antonio Pelusi <antoniopelusi2000@gmail.com>
2021-05-27 00:42:59 +0200

    Initial commit

---
+++ GamerAlley/posts/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'posts'
+    verbose_name = 'Post, comments and upvotes'
